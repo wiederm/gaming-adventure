8d33447d24d6b7c89629e88f1d2be5c3acc83f8e
Marcus Wieder <mwieder@Marcuss-MacBook-Air.local>
2025-12-23 16:41:41 +0100

    first attempt

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,99 @@
 use macroquad::prelude::*;
+use macroquad_tiled as tiled;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum GameState {
+    MainMenu,
+    Playing,
+    Paused,
+    GameOver,
+}
 
 #[macroquad::main("My game")]
 async fn main() {
+    // Any setup code goes here
+    // Load the texture
+    let mut game_state: GameState = GameState::MainMenu;
+    let tileset_texture = match load_texture("assets/maps/sheet.png").await {
+        Ok(tex) => tex,
+        Err(err) => {
+            eprintln!("Failed to load texture assets/maps/sheet.png: {err}");
+            return; // clean exit from main()
+        }
+    };
 
-    let mut x = screen_width() / 2.0;
-    let mut y = screen_height() / 2.0;
+    tileset_texture.set_filter(FilterMode::Nearest);
 
-    let mut i = 1;
+    // Load the map JSON
+    let tiled_map_json = match load_string("assets/maps/map.json").await {
+        Ok(s) => s,
+        Err(err) => {
+            eprintln!("Failed to load tiled map assets/maps/map.json: {err}");
+            return; // clean exit from main()
+        }
+    };
+
+    let sheet_tsj = match load_string("assets/maps/sheet.tsj").await {
+        Ok(s) => s,
+        Err(err) => {
+            eprintln!("Failed to load tileset assets/maps/sheet.tsj: {err}");
+            return;
+        }
+    };
+
+    let textures = &[("sheet.png", tileset_texture)];
+    let external_tilesets = &[("sheet.tsj", sheet_tsj.as_str())];
+
+    let tiled_map = match tiled::load_map(&tiled_map_json, textures, external_tilesets) {
+        Ok(m) => m,
+        Err(e) => {
+            eprintln!("Failed to parse/load tiled map: {e:?}");
+            return;
+        }
+    };
 
     loop {
-        i += 1;
+        let _dt = get_frame_time();
         clear_background(BLACK);
-        if is_key_down(KeyCode::Right) {
-            x += 1.0;
-        }
-        if is_key_down(KeyCode::Left) {
-            x -= 1.0;
-        }
-        if is_key_down(KeyCode::Down) {
-            y += 1.0;
-        }
-        if is_key_down(KeyCode::Up) {
-            y -= 1.0;
+
+        // Match game mode
+        match game_state {
+            GameState::MainMenu => {
+                // Render main menu
+                if is_key_pressed(KeyCode::Enter) {
+                    game_state = GameState::Playing;
+                    // score = 0 FIXME:
+                }
+                if is_key_pressed(KeyCode::Escape) {
+                    std::process::exit(0);
+                }
+
+                draw_text(
+                    "Press ENTER to Start",
+                    screen_width() / 2.0 - 100.0,
+                    screen_height() / 2.0,
+                    30.0,
+                    WHITE,
+                );
+            }
+            GameState::Playing => {
+                // Lets draw the whole map full screen
+                // Default macroquad camera is pixel perfect with (0, 0) in top left corner and (screen_width(), screen_height()) on bottom right
+                let dest_rect = Rect::new(0., 0., screen_width(), screen_height());
+
+                // And just draw our level!
+                tiled_map.draw_tiles("Tile Layer 1", dest_rect, None);
+
+                // Update and render game
+            }
+            GameState::Paused => {
+                // Render paused screen
+            }
+            GameState::GameOver => {
+                // Render game over screen
+            }
         }
 
-        let r: f32;
-        r = 16.0 * (i as f32 / 10.0).sin();
-
-        draw_circle(x, y, r, YELLOW);
         next_frame().await
     }
-
 }
